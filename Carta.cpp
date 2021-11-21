6f78a83f67d73c64793f9dd6addb3e4ead349df1
FrancoAltieri <94620664+FrancoAltieri@users.noreply.github.com>
2021-11-21 17:04:47 -0300

    Update Carta.cpp

--- Carta.cpp
+++ Carta.cpp
@@ -1,1 +1,25 @@
+#include "Carta.h"
+Carta::Carta(int numero) {
+	this->sig=NULL;
+	this->habilidad=numero;
+}
+int Carta::getHabilidad()
+{
+	return this->habilidad;
+}
+Carta* Carta::getSig()
+{
+	return this->sig;
+}
+void Carta::setHabilidad(int numero)
+{
+	this->habilidad=numero;
+}
+void Carta::setSig(Carta* carta)
+{
+	this->sig=carta;
+}
+Carta::~Carta() {
+
+}
 
