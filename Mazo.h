e744520d1fc9d5740b9d72059707de67b6bab760
FrancoAltieri <94620664+FrancoAltieri@users.noreply.github.com>
2021-11-27 16:08:52 -0300

    Update Mazo.h

--- Mazo.h
+++ Mazo.h
@@ -1,39 +1,14 @@
 #ifndef MAZO_H_
 #define MAZO_H_
+#include "Pila.h"
+#include "Carta.h"
 class Mazo {
 private:
-	Carta * p;
+	Pila<Carta>* mazo;
 public:
-	/*
-	 * Pre:-
-	 * post:crea un mazo
-	 */
 	Mazo();
-	/*
-	 * Pre:-
-	 * Post:Inserta una carta en el tope del mazo
-	 */
-	void push(int numero);
-	/*
-	 * Pre:-
-	 * Post:Elimina una carta en el tope del mazo y guarda la habilidad de la carta.
-	 */
-	void pop(int & x);
-	/*
-	 * Pre:-
-	 * Post:devuelve false si el mazo tiene elementos o true si esta vacio.
-	 */
-	bool estaVacia();
-	/*
-	 * Pre:-
-	 * Post:Devuelve el valor del ultimo elemento del mazo o 0 si no hay tope
-	 */
-	int getTope();
-	/*
-	 * Pre:-
-	 * Post:Destruye el mazo
-	*/
 	virtual ~Mazo();
+	void agregarCarta(Carta dato);
+	int sacarCarta(Carta &dato);
 };
-
 #endif /* MAZO_H_ */
