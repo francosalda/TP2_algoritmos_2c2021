085a892968df2d22f3004dd64dd417e99b497c6b
ezequielcano10 <emcano@fi.uba.ar>
2021-11-30 23:20:09 -0300

    destructor tablero

--- tablero.cpp
+++ tablero.cpp
@@ -21,114 +21,49 @@ Tablero::Tablero(int cantFilas, int cantColumnas, int cantEnProfundidad) {
             for (int k = 0; k < this->cantColumnas; k++)
             {
                 Casillero *casillero = new Casillero();
+                /*se eliminaron los parametros de  casillero ya que esyos no tenian uso*/
                 fila->agregar(casillero);
             }
             columna->agregar(fila);
         }
         this->casilleros->agregar(columna);
     }
-
-//ASIGNO LOS VECINOS
-// edit matriz vecinos // Algoritmo que Crea la matriz de vecinos // Terminado, falta implementar existeLaCasilla
-    for (int i = 0; i < this->cantColumnas; i++) { //el primer for me agarra la primer columna
-        for (int j = 0; j < this->cantFilas; j++) { //el segundo for me define la fila
-            for (int k = 0; k < this->cantEnProfundidad; k++) { //el 3er for la profundidad
-                Casillero *casillero = this->getCasilla(i, j, k);
-                for (int l = -1; l < 2; l++) {
-                    for (int m = -1; m < 2; m++) {
-                        for (int n = -1; n < 2; n++) {
-                            if (existeLaCasilla(i + l, j + m, k + n)) {   //Implementar existeLaCasilla (>0, <ancho, <alto, <profundo)
-                                //Con este if resolvemos la parte imaginaria(osea los que estarian en NULL porq estan por fuera del tablero)
-                                Casillero *casilleroVecino = this->getCasilla(i + l, j + m, k +n); //SI NO EXISTE LA CASILLA, getCasilla tendria q devolver excepcoion
-                                casilleroVecino->asignarVecino(l, m, n, casilleroVecino);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-//edit matriz vecinos
-
 }
 
+
 Tablero::~Tablero() {
+
+    this->casilleros->iniciarCursor();
+    while(this->casilleros->estaVacia()){
+
+        this->casilleros->obtenerCursor()->iniciarCursor();
+        while(this->casilleros->obtenerCursor()->estaVacia()){
+            (this->casilleros->obtenerCursor())->obtenerCursor()->iniciarCursor();
+            (this->casilleros->obtenerCursor())->avanzarCursor();    
+
+            while((this->casilleros->obtenerCursor())->obtenerCursor()->estaVacia()){
+                /*borrar memoria de ficha cuando  se pida la misma*/
+                delete (this->casilleros->obtenerCursor())->obtenerCursor()->obtenerCursor();
+                (this->casilleros->obtenerCursor())->obtenerCursor()->avanzarCursor();
+            }
+            delete this->casillero->obtenerCursor()->obtenerCursor()
+            this->casilleros->avanzarCursor();
+        }
+        delete this->casilleros->obtenerCursor();
+    }
+    delete this->casilleros;
 //destruir casilleros
 }
 
 Casillero *Tablero::getCasilla(int filaIntroducida, int columnaIntroducida, int profundidadIntroducida) { //me devuleve el dato en la posicion x y
     //validar
-    if((filaIntroducida<0) || (columnaIntroducida<0) || (profundidadIntroducida<0)){// || (filaIntroducida>this->x)) || (columnaIntroducida>this->y) ||(profundidadIntroducida>this->z){
-     //   string CoordInvalidas = "coordenadas de casilleros iunvalidas";
-       // throw Coordinvalidas;
+    if((filaIntroducida<0) || (columnaIntroducida<0) || (profundidadIntroducida<0) || (filaIntroducida>this->x)) || (columnaIntroducida>this->y) ||(profundidadIntroducida>this->z){
+        string CoordInvalidas = "coordenadas de casilleros iunvalidas";
+        throw Coordinvalidas;
     }
-   return this->casilleros->obtener(profundidadIntroducida)->obtener(filaIntroducida)->obtener(columnaIntroducida);
-   // return NULL;
+    return this->casilleros->obtener(cantFilas)->obtener(cantColumnas)->obtener(cantEnProfundidad);
 }
 
 void Tablero::setCasilla(int cantFilas, int cantColumnas, int cantEnProfundidad, char ficha) {
-    ((this->casilleros->obtener(cantFilas)->obtener(cantColumnas)->obtener(cantEnProfundidad)->setFicha(ficha)));
+    this->casilleros->obtener(cantFilas)->obtener(cantColumnas)->obtener(cantEnProfundidad)->setFicha(ficha);
 }
-
-
-void Tablero::crearMatrizVecinos(int cantFilas, int cantColumnas, int cantEnProfundidad) {
-
-    Casillero *casilla;
-
-    for (int i = 0; i < this->cantFilas; i++) {
-        for (int j = 0; j < this->cantColumnas; j++) {
-            for (int k = 0; k < this->cantEnProfundidad; k++) {
-
-                casilla = this->getCasilla(i,j,k);
-                Casillero**** matrizVecinal = new Casillero***();
-
-                    for (int l = 0; l <3; l++) {
-                        //Casillero *** columna = new Casillero**();
-                        matrizVecinal[l] = new Casillero** ();
-                        for (int m = 0; m < 3; m++) {
-                            matrizVecinal[l][m] = new Casillero*();
-                            for (int n = 0; n < 3; n++) {
-                                matrizVecinal[l][m][n] = new Casillero();
-                            }
-                        }
-                    }
-                    casilla->asignarMatrizVecinos(matrizVecinal);
-                }
-            }
-        }
-}
-
-bool Tablero::existeLaCasilla(int m, int n, int l) {
-    return true;
-}
-
-
-
-
-
-//edit matriz vecinos //TERMINAR!!
-  //Algoritmo que verifica el ganador
- /*
-bool Tablero::hayTateti(Casillero *casilleroOrigen, direccion) { //casilleroOrigen es donde estoy parado(revisar que seria)
-    casilleroOrigen->getVecinos();
-    int longitudesAdyacentes[3][3][3];
-
-    for (int i = 0; i <3 ; i++) {
-        for (int j = 0; j <3 ; j++) {
-            for (int k = 0; k <3 ; k++) {
-                longitudesAdyacentes[i][j][k] = casilleroOrigen->getLongitud(i,j,k); //Devuelve la longitud de los casiilrros usados x la misma ficha
-            }
-        }
-    }
-    for (int i = 0; i <3 ; i++) {
-        for (int j = 0; j <3 ; j++) {
-            for (int k = 0; k <3 ; k++) {
-                longitudesAdyacentes[i][j][k];
-
-            }
-        }
-    }
-}
-*/
-//edit matriz vecinos
