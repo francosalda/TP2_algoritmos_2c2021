2fc0048e85d4f497ed1ccb8c710a19463e46d81e
Franco <fsaldana@fi.uba.ar>
2021-12-17 13:13:17 -0300

    matriz de resultados cargada

--- tateti.cpp
+++ tateti.cpp
@@ -220,15 +220,13 @@ bool Tateti::hayGanador(int fil,int col,int prof)
 		}
 		cout<<"imprimiendo matriz de resultado :\n";
 		for(int k = 0 ; k < 3; k++)
-			{
-		for(int i = 0 ; i < 3; i++)
 		{
-			for(int j = 0 ; j < 3; j++)
+			for(int i = 0 ; i < 3; i++)
 			{
+				for(int j = 0 ; j < 3; j++)
+				{
 				
 					cout<<matrizResultados[i][j][k]<<",";
-				
-				
 			}cout<<endl;
 
 		}cout<<endl;
